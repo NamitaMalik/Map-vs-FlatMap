1c55857ab1020fc3c6dd3e172b52978dc8d29141
Namita Malik <namita1990@gmail.com>
2016-08-24 17:52:45 +0530

    Small demo

---
+++ rxdemo.js
@@ -0,0 +1,22 @@
+/**
+ * Created by namita on 8/24/16.
+ */
+
+//var Rx = require('rxjs/Rx');
+
+var a = [
+    "Namita",
+    "Amit",
+    "Rohit",
+    "Neetika"
+];
+
+var source1 = Rx.Observable.from(a)
+    .map(x=>Rx.Observable.of('Hello '+x))
+    .mergeAll();
+source1.subscribe(x=>document.getElementById('text').innerText = x);
+
+var source2 = Rx.Observable.from(a)
+    .flatMap(x=>Rx.Observable.of('Hello '+x));
+source2.subscribe(x=>console.log(x));
+
